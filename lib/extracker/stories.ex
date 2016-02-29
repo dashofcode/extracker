80fe4cb2f6d2cc2636eec53bc9117cd8508c36ed
Ricardo García Vega <bigardone@gmail.com>
2016-02-29 09:45:59 +0100

    Implemented find method on Stories module

--- lib/extracker/stories.ex
+++ lib/extracker/stories.ex
@@ -3,6 +3,22 @@ defmodule ExTracker.Stories do
   alias Extracker.Client
 
   @doc """
+  Get a single `story`.
+
+  ## Example
+
+      ExTracker.Stories.find("12345", "67890", client)
+
+  More info at: https://www.pivotaltracker.com/help/api/rest/v5#Story
+  """
+  @spec find(pos_integer, pos_integer, Client.t, [{atom, binary}] | []) :: ExTracker.Record.Story.t
+  def find(project_id, story_id, client, params \\ []) do
+    get("projects/#{project_id}/stories/#{story_id}", client, params)
+    |> ExTracker.Parser.parse_story
+  end
+
+
+  @doc """
   Get all stories from project
 
   ## Example
